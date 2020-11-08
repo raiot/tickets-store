dd9edc069495c2d08c03c64117297c08e671c45a
Rene Durazo <adurazo@nearsoft.com>
2020-11-08 13:03:40 -0600

    missing fix for rating filter

--- src/components/Discovery/Discovery.tsx
+++ src/components/Discovery/Discovery.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useStore } from '../../store';
 import MovieService from '../../services/MovieService';
 
@@ -10,6 +10,7 @@ const filter = (movie: IMovie, stars: number | null) => (stars ? Math.floor(movi
 
 const Discovery: React.FC = () => {
     const { state, dispatch } = useStore();
+    const [visible, setVisible] = useState(0);
 
     useEffect(() => {
         dispatch({ type: 'LOADING' });
@@ -21,13 +22,46 @@ const Discovery: React.FC = () => {
             getMovies();
     }, [dispatch]);
 
+    const handleNav = (direction: string, max: number) => (e: any) => {
+        if(direction === "right") {
+            if (visible < max) {
+                setVisible(visible + 1);
+            }
+        } else {
+            if(visible > 0) {
+                setVisible(visible - 1);
+            }
+        }
+    }
     const theMovies = state.filter ? state.movies.filter((m:IMovie) => filter(m, state.filter)) : state.movies;
+    const grouped = theMovies.reduce((acc: Array<Array<IMovie>> | Array<Array<never>>, movie: IMovie) => {
+        if(acc.length === 0) {
+            acc.push([]);
+        }
+
+        if(acc[acc.length - 1].length < 4) {
+            acc[acc.length - 1].push(movie);
+        } else {
+            acc.push([]);
+            acc[acc.length - 1].push(movie);
+        }
+        return acc;
+    }, []);
 
     return(
         <section>
             <h2>Popular Movies</h2>
             <div className="discovery">
-                {theMovies.map((movie: IMovie, index: number): any => <Movie key={index} {...movie} index={index} />) }
+                <button onClick={handleNav('left', grouped.length - 1)} type="button" className="nav left">{'<'}</button>
+                {
+                    grouped
+                    .map((movies: Array<IMovie>, index: number): any =>
+                    <div key={`group-${index}`} className={`layer ${visible === index ? 'visible' : ''}`} data-scene={index}>
+                      {movies.map((movie: IMovie, index: number) => <Movie key={`movie-${index}`} {...movie} index={index} />)}
+                    </div>
+                    )
+                }
+                <button onClick={handleNav('right', grouped.length - 1)} type="button" className="nav right">{'>'}</button>
             </div>
         </section>
     );
